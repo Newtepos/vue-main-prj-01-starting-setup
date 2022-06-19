c47d59310f46d9589498a48b7380c2ed5c231217
Supakit <supakit.pnm@gmail.com>
2022-06-19 10:22:04 +0700

    Fix: Route Path Typo

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
-import { createRoute, createWebHistory } from 'vue-router';
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetails.vue';
 import CoachRegister from './pages/coaches/CoachRegister.vue';
-import CoachList from './pages/coaches/CoachList.vue';
+import CoachList from './pages/coaches/CoachsList.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestedRecevied from './pages/requests/RequestedRecevied.vue';
+import RequestedRecevied from './pages/requests/RequestsRecevied.vue';
 import NotFound from './pages/NotFound.vue'
 
-const router = createRoute({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
