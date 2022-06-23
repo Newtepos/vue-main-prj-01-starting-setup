2ca480ace6c9c81bedfcf1e4fd0bbd902972662d
Supakit <supakit.pnm@gmail.com>
2022-06-23 15:01:24 +0700

    Add: Async Component

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import CoachDetail from './pages/coaches/CoachDetails.vue';
-import CoachRegister from './pages/coaches/CoachRegister.vue';
+// import CoachDetail from './pages/coaches/CoachDetails.vue';
+// import CoachRegister from './pages/coaches/CoachRegister.vue';
 import CoachList from './pages/coaches/CoachsList.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestedRecevied from './pages/requests/RequestsRecevied.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestedRecevied from './pages/requests/RequestsRecevied.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegister = () => import('./pages/coaches/CoachRegister.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestedRecevied = () => import('./pages/requests/RequestsRecevied.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
